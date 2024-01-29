3403dfb963a2f7e07cdf1b0e46a50d96591ad173
Alpetpon <ALpetpon51pro@gmail.com>
2024-01-30 00:45:35 +0300

    Update 2.8 for 2.3

--- lab_2/8.cpp
+++ lab_2/8.cpp
@@ -1,38 +1,87 @@
 #include <iostream>
 
-class Shape {
-public:
-    virtual ~Shape() {}
+enum Color { RED, GREEN, BLUE };
 
-    // Чисто виртуальная функция
-    virtual void Inflate(int factor) = 0;
+class Shape {
+protected:
+    Color color;
+
+public:
+    Shape(Color c) : color(c) {}
+
+    Color getColor() const {
+        return color;
+    }
+
+    void WhereAmI() const {
+        std::cout << "Now I am in class Shape" << std::endl;
+    }
+
+    virtual void WhereAmIVirtual() const {
+        std::cout << "Now I am in class Shape (virtual)" << std::endl;
+    }
+
+        
+    virtual void Inflate(int factor) = 0; 
+    
+    virtual ~Shape() {
+        std::cout << "Now I am in Shape's destructor!" << std::endl;
+    }
+
+    
 };
 
 class Rect : public Shape {
 public:
-    // Реализация виртуальной функции для класса Rect
+    Rect(Color c = RED) : Shape(c) {}
+    
+    void WhereAmI() const {
+        std::cout << "Now I am in class Rect" << std::endl;
+    }
+
+    void WhereAmIVirtual() const override {
+        std::cout << "Now I am in class Rect (virtual)" << std::endl;
+    }
+    
     void Inflate(int factor) override {
-        std::cout << "Inflating Rect by factor: " << factor << std::endl;
+        std::cout << "Rect is inflating by a factor of " << factor << std::endl;
+    }
+
+    ~Rect() {
+        std::cout << "Now I am in Rect's destructor!" << std::endl;
     }
 };
 
 class Circle : public Shape {
 public:
-    // Реализация виртуальной функции для класса Circle
+    Circle() : Shape(RED) {}
+
+    Circle(const Rect& rect) : Shape(rect.getColor()) {}
+
+    void WhereAmI() const {
+        std::cout << "Now I am in class Circle" << std::endl;
+    }
+
+    void WhereAmIVirtual() const override {
+        std::cout << "Now I am in class Circle (virtual)" << std::endl;
+    }
+    
     void Inflate(int factor) override {
-        std::cout << "Inflating Circle by factor: " << factor << std::endl;
-        
+        std::cout << "Circle is inflating by a factor of " << factor << std::endl;
+    }
+
+    ~Circle() {
+        std::cout << "Now I am in Circle's destructor!" << std::endl;
     }
 };
 
 int main() {
     Rect r;
     Shape* p = &r;
-    p->Inflate(5);  // Вызовется метод Inflate() класса Rect
-
+    p->Inflate(5); 
+    
     Circle c;
     p = &c;
-    p->Inflate(5);  // Вызовется метод Inflate() класса Circle
-
+    p->Inflate(5);
     return 0;
 }
