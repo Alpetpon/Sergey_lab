bdd572c576b778316eaa9f82cbbfe34526a4aeee
Alpetpon <ALpetpon51pro@gmail.com>
2024-01-30 00:40:58 +0300

    Update 2.7 for 2.3

--- lab_2/7.cpp
+++ lab_2/7.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
-enum Color {
-    RED,
-    GREEN,
-    BLUE
-};
+enum Color { RED, GREEN, BLUE };
 
 class Shape {
 protected:
@@ -13,10 +9,20 @@ protected:
 public:
     Shape(Color c) : color(c) {}
 
-    virtual ~Shape() {}
+    Color getColor() const {
+        return color;
+    }
 
-    void WhereAmIVirtual() {
+    void WhereAmI() const {
+        std::cout << "Now I am in class Shape" << std::endl;
+    }
+
+    virtual void WhereAmIVirtual() const {
         std::cout << "Now I am in class Shape (virtual)" << std::endl;
+    }
+
+    virtual ~Shape() {
+        std::cout << "Now I am in Shape's destructor!" << std::endl;
     }
 };
 
@@ -24,21 +30,46 @@ class Rect : public Shape {
 public:
     Rect(Color c) : Shape(c) {}
 
-    ~Rect() override {}
+    void WhereAmI() const {
+        std::cout << "Now I am in class Rect" << std::endl;
+    }
 
-    void WhereAmIVirtual()  {
+    void WhereAmIVirtual() const override {
         std::cout << "Now I am in class Rect (virtual)" << std::endl;
+    }
+
+    ~Rect() {
+        std::cout << "Now I am in Rect's destructor!" << std::endl;
+    }
+};
+
+class Circle : public Shape {
+public:
+    Circle() : Shape(RED) {}
+
+    Circle(const Rect& rect) : Shape(rect.getColor()) {}
+
+    void WhereAmI() const {
+        std::cout << "Now I am in class Circle" << std::endl;
+    }
+
+    void WhereAmIVirtual() const override {
+        std::cout << "Now I am in class Circle (virtual)" << std::endl;
+    }
+
+    ~Circle() {
+        std::cout << "Now I am in Circle's destructor!" << std::endl;
     }
 };
 
 int main() {
     Rect r(GREEN);
     Shape* p = &r;
-
-    p->WhereAmIVirtual(); // Now I am in class Shape (virtual)
-
+    
+    p->WhereAmIVirtual();             // Now I am in class Shape (virtual)
+    
     // Оператор разрешения области видимости для вызова WhereAmIVirtual() из класса Shape
-    r.Shape::WhereAmIVirtual(); // Now I am in class Shape (virtual)
+    r.Shape::WhereAmIVirtual();       // Now I am in class Shape (virtual)
 
     return 0;
 }
