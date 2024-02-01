f80700ca40ba84db4be7b88d5e60e08f14bcefe9
Alpetpon <ALpetpon51pro@gmail.com>
2024-02-02 00:56:41 +0300

    Update 1.4

--- lab_1/4.cpp
+++ lab_1/4.cpp
@@ -19,6 +19,19 @@ public:
     void PrintRect() const {
         std::cout << "Rect: x=" << x << ", y=" << y << ", width=" << width << ", height=" << height << std::endl;
     }
+    
+    // Задание 4б. Передача объектов по ссылке.
+    Rect BoundingRect3(const Rect& rect1, const Rect& rect2) {
+        // Здесь объекты передаются по ссылке, и конструкторы копирования не вызываются
+        int minX = std::min(rect1.x, rect2.x);
+        int minY = std::min(rect1.y, rect2.y);
+        int maxX = std::max(rect1.x + rect1.width, rect2.x + rect2.width);
+        int maxY = std::max(rect1.y + rect1.height, rect2.y + rect2.height);
+        
+        return Rect(minX, minY, maxX - minX, maxY - minY);
+    }
+
+    
 };
 
 // Глобальная функция BoundingRect
@@ -32,9 +45,9 @@ Rect BoundingRect(Rect rect1, Rect rect2) {
     return Rect(minX, minY, maxX - minX, maxY - minY);
 }
 
-// Задание 4а. Передача объектов по ссылке.ы
+// Задание 4а. Передача объектов по ссылке.
 Rect BoundingRect2(const Rect& rect1, const Rect& rect2) {
-    // Здесь объекты передаются по ссылке, и конструкторы копирования не вызываются
+    // Здесь объекты передаются по ссылке, и коныструкторы копирования не вызываются
     int minX = std::min(rect1.x, rect2.x);
     int minY = std::min(rect1.y, rect2.y);
     int maxX = std::max(rect1.x + rect1.width, rect2.x + rect2.width);
@@ -49,8 +62,11 @@ int main() {
     // Глобальная функция BoundingRect
     r3 = BoundingRect(r1, r2);
     r3.PrintRect();
-
+    // Глобальная функция BoundingRect2
     r3 = BoundingRect2(r1, r2);
+    r3.PrintRect();
+    // функция BoundingRect3 как метод класса
+    r3.BoundingRect3(r1, r2);
     r3.PrintRect();
 
     return 0;
