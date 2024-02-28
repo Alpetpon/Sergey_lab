8333621d456f02428cf4692fc4f0ebca4dc67018
Alex_Chpock <99286317+Alpetpon@users.noreply.github.com>
2024-02-28 20:19:05 +0300

    lab_6

--- lab_6/Counter.h
+++ lab_6/Counter.h
@@ -3,30 +3,52 @@
 
 #include <cstring>
 
+// Класс Counter представляет собой умный указатель с подсчетом ссылок для строковых данных.
 class Counter {
 private:
-    char* m_pStr;
-    int m_nOwners;
-    Counter* pNext;
-    int* m_pRefCount;
+    char* m_pStr;            // Указатель на строку данных
+    int m_nOwners;           // Количество владельцев данного экземпляра
+    Counter* pNext;          // Указатель на следующий экземпляр Counter в списке
+    int* m_pRefCount;        // Указатель на счетчик ссылок для данной строки
 
-    static Counter* m_pHead;
-    static unsigned int m_curCounters;
-
+    static Counter* m_pHead;             // Голова списка экземпляров Counter
+    static unsigned int m_curCounters;   // Общее количество счетчиков
 
 public:
+    // Конструктор, создающий экземпляр Counter для переданной строки данных.
     Counter(const char* str);
+
+    // Деструктор, освобождающий ресурсы, связанные с экземпляром Counter.
     ~Counter();
+
+    // Увеличивает количество владельцев данного экземпляра Counter.
     void AddUser();
+
+    // Уменьшает количество владельцев данного экземпляра Counter.
     void RemoveUser();
 
+    // Статический метод для поиска экземпляра Counter по строке данных.
     static Counter* Find(const char* str);
+
+    // Получает указатель на голову списка экземпляров Counter.
     static Counter* GetHead() { return m_pHead; }
+
+    // Получает указатель на следующий экземпляр Counter в списке.
     Counter* GetNext() const { return pNext; }
+
+    // Получает текущее количество активных счетчиков.
     static unsigned int GetCurCounters() { return m_curCounters; }
+
+    // Получает строку данных, связанную с данным экземпляром Counter.
     char* GetString() const { return m_pStr; }
+
+    // Получает количество владельцев данного экземпляра Counter.
     int GetOwners() const { return m_nOwners; }
+
+    // Увеличивает количество владельцев данного экземпляра Counter на единицу.
     void IncrementOwners() { ++m_nOwners; }
+
+    // Уменьшает количество владельцев данного экземпляра Counter на единицу.
     void DecrementOwners() { --m_nOwners; }
 };
 
--- lab_6/myString.h
+++ lab_6/myString.h
@@ -3,32 +3,36 @@
 
 #include <iostream>
 #include "Counter.h"
+
+// Класс MyString представляет строку с подсчетом ссылок.
 class MyString {
 private:
-    char* m_str;
-    Counter* m_counter;
+    char* m_str;         // Указатель на символьный массив (строку)
+    Counter* m_counter;  // Указатель на экземпляр Counter для управления подсчетом ссылок
 
 public:
-    MyString();
-    MyString(const char* str);
-    MyString(const MyString& other);
-    ~MyString();
+    // Конструкторы и деструктор
+    MyString();                              // Конструктор по умолчанию
+    MyString(const char* str);               // Конструктор с параметром (создает строку из переданной строки)
+    MyString(const MyString& other);         // Конструктор копирования
+    ~MyString();                             // Деструктор
 
-    const char* GetString() const;
-    void SetNewString(const char* newStr);
-    MyString& operator=(const MyString& other);
+    // Методы доступа к данным
+    const char* GetString() const;           // Получить строку
+    void SetNewString(const char* newStr);   // Установить новую строку
 
-    void AddUser();
-    friend std::ostream& operator<<(std::ostream& os, const MyString& myString);
-    MyString operator+(const MyString& other) const;
-    MyString& operator+=(const MyString& other);
+    // Перегруженные операторы
+    MyString& operator=(const MyString& other);               // Оператор присваивания
+    friend std::ostream& operator<<(std::ostream& os, const MyString& myString);  // Оператор вывода
+    MyString operator+(const MyString& other) const;          // Оператор сложения строк
+    MyString& operator+=(const MyString& other);              // Оператор добавления строк
+    MyString& operator++();                                   // Оператор инкремента
+    MyString operator--(int);                                // Оператор декремента
 
-    MyString& operator++();
-    MyString operator--(int);
-    static void print();
-
-    static void changeCase();
-    static void printAlph();
+    // Статические методы
+    static void print();               // Вывести текущее количество строк и счетчиков
+    static void changeCase();          // Изменить регистр всех символов во всех строках
+    static void printAlph();           // Вывести все строки в алфавитном порядке
 };
 
 #endif
